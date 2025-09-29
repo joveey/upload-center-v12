cd533b287cce871e8d28ad0eaeb2376f74975c4d
joveey <jovisywl@gmail.com>
2025-09-29 14:44:30 +0700

    Add: MappingController and routes for register format feature

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MappingController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,22 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mapping Routes
+    |--------------------------------------------------------------------------
+    | Rute untuk fitur inti Upload Center Cerdas.
+    | Dilindungi oleh permission 'register format'.
+    |
+    */
+    Route::middleware('can:register format')->group(function () {
+        Route::get('/register-format', [MappingController::class, 'showRegisterForm'])->name('mapping.register.form');
+        Route::post('/register-format', [MappingController::class, 'processRegisterForm'])->name('mapping.register.process');
+        Route::get('/register-format/map', [MappingController::class, 'showMapForm'])->name('mapping.map.form');
+        Route::post('/register-format/map', [MappingController::class, 'storeMapping'])->name('mapping.map.store');
+    });
+
 });
 
 require __DIR__.'/auth.php';
